e5db091b45ceba973a493d4c5a344ca504e41e69
Alex Ledesma <alex.ledesma7@gmail.com>
2022-07-31 23:36:43 -0400

    adding verbose argument to cli

--- cli_tutorial/cli.py
+++ cli_tutorial/cli.py
@@ -1,6 +1,8 @@
 __version__ = '0.1.0'
 
 import click
+from funcy import identity
+
 @click.command()
 @click.option("--in", "-i", "in_file", required=True,
     help="Path to csv file to be processed.",
@@ -10,12 +12,17 @@ import click
     help="Path to excel file to store the result.",
     type=click.Path(dir_okay=False)
 )
-def process(in_file, out_file):
-    """ Processes the input file IN and stores the result to 
+@click.option('--verbose', is_flag=True, help="Verbose output")
+def process(in_file, out_file, verbose):
+    """ Processes the input file IN and stores the result to
     output file OUT.
     """
+    print_func = print if verbose else identity
+    print_func("We will start with the input")
     input = read_csv(in_file)
+    print_func("Next we procees the data")
     output = process_csv(input)
+    print_func("Finally, we dump it")
     write_excel(output, out_file)
 
 if __name__ =="__main__":
