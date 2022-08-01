99ef7e48881b23ed25c420b3c9cc4757d659c08e
Alex Ledesma <alexledesma@Alexs-MBP.lan>
2022-07-31 23:21:22 -0400

    adding type requirement to input and output to validate path's submitted by user

--- cli_tutorial/cli.py
+++ cli_tutorial/cli.py
@@ -4,9 +4,12 @@ import click
 @click.command()
 @click.option("--in", "-i", "in_file", required=True,
     help="Path to csv file to be processed.",
+    type=click.Path(exists=True, dir_okay=False, readable=True)
 )
 @click.option("--out-file", "-o", default="./output.xlsx",
-    help="Path to excel file to store the result.")
+    help="Path to excel file to store the result.",
+    type=click.Path(dir_okay=False)
+)
 def process(in_file, out_file):
     """ Processes the input file IN and stores the result to 
     output file OUT.
