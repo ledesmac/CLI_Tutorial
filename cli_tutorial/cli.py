1144044cc5858a4d6e7656d54e27debc8c6d7133
Alex Ledesma <alexledesma@Alexs-MBP.lan>
2022-07-31 23:06:56 -0400

    creating initial skeleton for CLI

--- cli_tutorial/cli.py
+++ cli_tutorial/cli.py
@@ -1,1 +1,19 @@
 __version__ = '0.1.0'
+
+import click
+@click.command()
+@click.option("--in", "-i", "in_file", required=True,
+    help="Path to csv file to be processed.",
+)
+@click.option("--out-file", "-o", default="./output.xlsx",
+    help="Path to excel file to store the result.")
+def process(in_file, out_file):
+    """ Processes the input file IN and stores the result to 
+    output file OUT.
+    """
+    input = read_csv(in_file)
+    output = process_csv(input)
+    write_excel(output, out_file)
+
+if __name__ =="__main__":
+    process()
